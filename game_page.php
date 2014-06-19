258b9ebd53ea6ab7d584cdbe2adab0d26a02c47f
Maël Schweighardt <mael.schweighardt@epitech.eu>
2014-06-19 16:12:47 +0200

    Brunne Swag

--- game_page.php
+++ game_page.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<head>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <link rel="stylesheet" href="game_page.css" />
-<head>
+<link rel="icon" type="image/png" href="favicon.png" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Accueil</title>
+<title>Country Click - Alpha 0.1</title>
 </head>
 
 <body>
 <header>
-  <h1><a href="index.html" title="Index">Country Click</a></h1>
+  <h1><a href="index.php" title="Index">Country Click</a></h1>
+<?php include("menu.php"); ?>
 </header>
 <div id="left">
 </div>
