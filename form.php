e16d3fa786148c4649183654ed272718468c5f46
Maël Schweighardt <mael.schweighardt@epitech.eu>
2014-06-19 16:23:09 +0200

    update

--- form.php
+++ form.php
@@ -1,28 +1,40 @@
 <aside>
   <h1>Inscription</h1>
-  <label for="pseudo">Pseudo : </label>
-  <input type="text" name="pseudo" id="pseudo" />
-  <br />
-  <br />
-  <label for="email">E-mail : </label>
-  <input type="email" name="email" id="email" />
-  <br />
-  <br />
-  <label for="pwd">Mot de passe : </label>
-  <input type="password" name="pwd" id="pwd" />
-  <br />
-  <br />
-  <input type="submit" value="Valider" />
-  <br/>
+  <table>
+    <tr class="ligne">
+      <td class="libelle">Pseudo :</td>
+      <td><input type="text" name="pseudo" value=""/></td>
+    </tr>
+    <tr class="ligne">
+      <td class="libelle">E-mail :</td>
+      <td><input type="mail" name="mail" value=""/></td>
+    </tr>
+    <tr class="ligne">
+      <td class="libelle">Mot de passe :</td>
+      <td><input type="password" name="password" value=""/></td>
+    </tr>
+    <tr class="ligne">
+      <td class="libelle">Confirmez le mot de passe :</td>
+      <td><input type="password" name="password2" value=""/></td>
+    </tr>
+  </table>
   <h1>Connexion</h1>
-  <label for="pseudo">Pseudo ou e-mail : </label>
-  <input type="text" name="pseudo" id="pseudo" />
-  <br />
-  <br />
-  <label for="pseudo">Pseudo : </label>
-  <input type="text" name="pseudo" id="pseudo" />
-  <br />
-  <br />
-  <input type="submit" value="Valider" />
-  <br/>
+  <table>
+    <tr class="ligne">
+      <td class="libelle">Pseudo :</td>
+      <td><input type="text" name="pseudo" value=""/></td>
+    </tr>
+    <tr class="ligne">
+      <td class="libelle">E-mail :</td>
+      <td><input type="mail" name="mail" value=""/></td>
+    </tr>
+    <tr class="ligne">
+      <td class="libelle">Mot de passe :</td>
+      <td><input type="password" name="password" value=""/></td>
+    </tr>
+    <tr class="ligne">
+      <td class="libelle">Confirmez le mot de passe :</td>
+      <td><input type="password" name="password2" value=""/></td>
+    </tr>
+  </table>
 </aside>
