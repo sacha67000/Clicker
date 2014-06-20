6939347e3ff81ea4254cf9b53a4cfde1eb032022
Maël Schweighardt <mael.schweighardt@epitech.eu>
2014-06-20 10:07:41 +0200

    Bouton

--- game_page.php
+++ game_page.php
@@ -1,37 +1,38 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <head>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<link rel="stylesheet" href="game_page.css" />
-<link rel="icon" type="image/png" href="favicon.png" />
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Country Click - Alpha 0.1</title>
+	<html xmlns="http://www.w3.org/1999/xhtml">
+	<link rel="stylesheet" href="game_page.css" />
+	<link rel="icon" type="image/png" href="favicon.png" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Country Click - Alpha 0.1</title>
 </head>
 
 <body>
-<header>
-  <h1><a href="index.php" title="Index">Country Click</a></h1>
-<?php include("menu.php"); ?>
-</header>
-<div id="left">
-<h1>Vos ressources</h1>
-<h2>42 $</h2>
-<div id="dollar">
-<img src="img/dollar.png" alt="dollar" />
-</div>
-<h4>28 $ par seconde</h4>
-<h3>TOTAL: 2 $</h3>
-</div>
+	<header>
+		<h1><a href="index.php" title="Index">Country Click</a></h1>
+		<?php include("menu.php"); ?>
+	</header>
+	<div id="left">
+		<div id="dollar_panel">
+			<h1>Vos ressources</h1>
+			<h2>42 $</h2>
+			<div id="dollar">
+				<img src="img/dollar.png" alt="dollar" />
+			</div>
+			<h4>28 $ par seconde</h4>
+			<h3>TOTAL: 2 $</h3>
+		</div>
+		<div id="save_button">
+			<img src="img/disquette.png" alt="disquette" />
+		</div>
+	</div>
 
-<div id="save_button">
-<img src="img/disquette.png" alt="disquette" />
-</div>
+	<div id="central">
+	</div>
 
-<div id="central">
-</div>
-
-<aside>
-  <h1>Right_DIV</h1>
-</aside>
+	<aside>
+		<h1>Right_DIV</h1>
+	</aside>
 
 </body>
 </html>
--- inscription.php
+++ inscription.php
@@ -1,5 +1,18 @@
-<?php echo "Votre pseudo est :", $_POST['mail']; 
+<?php echo "Votre pseudo est : ", $_POST['pseudo'], "<br />Votre mail est : ", $_POST['mail'], "<br />Votre mot de passe est : ", sha1($_POST['password']), "<br />";
 
-header('Location: index.php');
+if ($_POST['password'] == $_POST['password2'])
+{
+	try {
+		$bdd = new PDO('mysql:host=localhost;dbname=countryclicker', 'root', '');
+	}
+	catch(Exception $e)
+	{
+		echo "Bonjour";
+		die('Erreur : '.$e->getMessage());
+	}
+
+}
+else
+	echo "Les mots de passe ne correspondent pas";
 ?>
 
